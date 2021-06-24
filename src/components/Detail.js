7ce783cc01cb7b5a2d1bc0a35bd50f82317bbe61
Sae0524 <ume.m3s@gmail.com>
2021-06-24 13:42:29 -0700

    first commit

---
+++ src/components/Detail.js
@@ -0,0 +1,18 @@
+import React from "react"
+import './Detail.css';
+
+
+function Detail(){
+  return (
+    <div className="detail">
+       <h2>Title of the house</h2>
+       <img src="https://a0.muscache.com/im/pictures/dc09ee21-27e9-4dcd-9b59-7ba7ade0563f.jpg?im_w=1200" alt=""/>
+       <div>
+         <p>Channel the pursuit of pause in this one-of-a-kind earth house. The cozy retreat was hand-sculpted using  local   and    sustainable natural materials, and features a central living space with cantilevered slab stairs  leading to   the loft    bedroom.</p>
+         <p>$60 /night</p>
+       </div>
+    </div>
+  )
+}
+
+export default Detail;
